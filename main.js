e54e7338f7782af61e8baa18dd9f805e5878fcfd
Tech Geek <martezjmartez@outlook.com>
2025-03-17 16:02:16 -0500

    ASSIGNMENT COMPLETE

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
+function moreThan5(number){
+  return number > 5
+}
+
+function isNewTopScore(score, topScore){
+  return score > topScore
+}
+
 function isInDanger(grade){
   const result = 60 <= grade && grade <= 71
   return result
+}
+
+function isCoasting(grade){
+  const result = 72 <= grade && grade <=83
+  return result
+}
+
+function  isSucceeding(grade){
+  const result = 84 <= grade && grade <= 92
+  return result
+}
+
+function isFailing(grade){
+  return grade < 60
+}
+
+function isAcing(grade){
+  return grade > 92
+} 
+
+function isStudent(role){
+  return role === "student"
+}
+
+function isTeacher(role){
+  return role === "teacher"
+}
+
+function isAdmin(role){
+  return role === "admin"
+}
+
+function isElementary(role){
+  return role === "elementary"
+}
+
+function areDifferentPeople(name1, name2) {
+  return name1 !== name2
+}
+
+function isMiddleSchoolTeacher(role, level){
+  return role === "teacher" && level >= 6 && level <=8
+}
+
+function notAnElementarySchoolAdministrator(level, role){
+  return level !== "elementary" || role !== "administrator"
 }
 
 
